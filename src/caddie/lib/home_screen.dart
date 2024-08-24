970921eaf802531e53af52898364779c70cd4a45
Malo-Archimbaud <malo.archimbaud@algosup.com>
2024-08-24 19:57:33 +0200

    reworked first screen

--- src/caddie/lib/home_screen.dart
+++ src/caddie/lib/home_screen.dart
@@ -16,30 +16,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: Stack(children: [
+          Positioned.fill(child: Image.asset('assets/background_golf_1.jpg', fit: BoxFit.cover)),
+      Column(
+        children: [
+          const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [AppBarWidget()]),
+          const Expanded(child: Column(),),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Nouvelle partie'),
+                      content: const Text(
+                          'Voulez-vous commencer une nouvelle partie ?'),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Non')),
+                        TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              Navigator.of(context).pushNamed('/newGame');
+                            },
+                            child: const Text('Oui'))
+                      ],
+                    );
+                  });
+            },
+            child: const Text('Commencer une nouvelle partie'),
+          ),
+        ],
+      ),
+    ]));
+  }
+
+  Widget newGameScreen() {
+    return Scaffold(
         body: Center(
       child: Column(
         children: [
           const Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [AppBarWidget()]),
-          const Text('Home Screen'),
-          FutureBuilder<Position>(
-            future: _position,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return Text(
-                      'Location: ${snapshot.data?.latitude}, ${snapshot.data?.longitude}');
-                }
-              } else {
-                return const CircularProgressIndicator();
-              }
+          const Text('Nouvelle partie'),
+          ElevatedButton(
+            onPressed: () {
+              // add logic
             },
-          ),
+            child: const Text('Start New Game'),
+          )
         ],
       ),
     ));
   }
 }
+
+/*FutureBuilder<Position>(
+            future: _position,
+            builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return Text(
+                    'Position: ${snapshot.data!.latitude}, ${snapshot.data!.longitude}');
+              }
+            },
+          ),*/
