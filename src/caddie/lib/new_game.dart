248087f3f1c55244b7d5abb722cf8a85b628a1db
Malo-Archimbaud <malo.archimbaud@algosup.com>
2024-09-02 01:52:32 +0200

    Say which club to pick

--- src/caddie/lib/new_game.dart
+++ src/caddie/lib/new_game.dart
@@ -5,6 +5,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:caddie/localisation.dart';
 import 'package:caddie/widgets/app_bar_widget.dart';
 import 'point.dart';
+import 'player.dart';
 
 class NewGameScreen extends StatefulWidget {
   const NewGameScreen({super.key});
@@ -14,113 +15,200 @@ class NewGameScreen extends StatefulWidget {
 }
 
 class NewGameScreenState extends State<NewGameScreen> {
-  final Future<Position> _position = determinePosition();
+  late Future<Position> _position;
+  late Player player;
   List<Point> area = [];
   String golfCourse = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _position = determinePosition();
+    _loadPlayerData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(children: [
-        const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [AppBarWidget()]),
-        FutureBuilder<String>(
-          future: compareCoordinates(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              late double latitude = 0;
-              late double longitude = 0;
-              golfCourse = snapshot.data!;
-              return FutureBuilder<List<Point>>(
-                future: loadPointsFromJsonFile(golfCourse),
-                builder: (context, snapshot) {
-                  Point p;
-                  if (snapshot.hasData) {
-                    area = snapshot.data!;
-                    _position.then((position) {
-                      latitude = position.latitude;
-                      longitude = position.longitude;
-                    });
-                    if (latitude != 0 && longitude != 0) {
-                      p = Point(latitude, longitude);
-                      bool b = isPointInArea(p, area);
-                      
-                      return Text('Golf course: $b');
-                    }
-                  } else if (snapshot.hasError) {
-                    return Text('${snapshot.error}');
-                  }
+        child: Column(
+          children: [
+            const AppBarWidget(),
+            FutureBuilder<String>(
+              future: _getNearestGolfCourse(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
                   return const CircularProgressIndicator();
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
-            return const CircularProgressIndicator();
-          },
+                }
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                if (snapshot.hasData) {
+                  golfCourse = snapshot.data!;
+                  return _buildGolfCourseInfo();
+                }
+                return const Text('No golf course found nearby.');
+              },
+            ),
+          ],
         ),
-      ])),
+      ),
     );
   }
 
-  Future<String> compareCoordinates() async {
-    final jsonCoordinates = await readCoordinatesFromJson();
-    String name = '';
-    return _position.then((position) {
-      final currentLatitude = position.latitude;
-      final currentLongitude = position.longitude;
+  Future<String> _getNearestGolfCourse() async {
+    final coordinates = await _loadCoordinatesFromJson();
+    final position = await _position;
+    String nearestCourse = '';
+    double minDistance = double.infinity;
 
-      for (final entry in jsonCoordinates.entries) {
-        final jsonCoordinate = entry.value;
-        final jsonLatitude = jsonCoordinate['latitude'];
-        final jsonLongitude = jsonCoordinate['longitude'];
+    for (var entry in coordinates.entries) {
+      final distance = Geolocator.distanceBetween(
+        position.latitude,
+        position.longitude,
+        entry.value['latitude']!,
+        entry.value['longitude']!,
+      );
 
-        final distance = Geolocator.distanceBetween(
-            currentLatitude, currentLongitude, jsonLatitude!, jsonLongitude!);
-        if (distance < 1000) {
-          name = entry.key;
-        }
+      if (distance < minDistance) {
+        minDistance = distance;
+        nearestCourse = entry.key;
       }
-      return name;
-    });
+    }
+
+    return nearestCourse;
   }
 
-  Future<Map<String, Map<String, double>>> readCoordinatesFromJson() async {
+  Future<Map<String, Map<String, double>>> _loadCoordinatesFromJson() async {
     try {
-      String data = await rootBundle.loadString('assets/golf_coordinates.json');
+      final data = await rootBundle.loadString('assets/golf_coordinates.json');
       final Map<String, dynamic> jsonData = jsonDecode(data);
 
-      final Map<String, Map<String, double>> golfCourseDict = {};
-      for (var course in jsonData['golf_courses']) {
-        final String name = course['name'];
-        final double latitude = course['coordinates']['latitude'];
-        final double longitude = course['coordinates']['longitude'];
-        golfCourseDict[name] = {
-          'latitude': latitude,
-          'longitude': longitude,
-        };
-      }
-      return golfCourseDict;
+      return {
+        for (var course in jsonData['golf_courses'])
+          course['name']: {
+            'latitude': course['coordinates']['latitude'],
+            'longitude': course['coordinates']['longitude'],
+          }
+      };
     } catch (e) {
-      print('Error loading golf courses from assets: $e');
-      return {};
+      throw Exception('Error loading golf courses from assets: $e');
     }
   }
 
-  Future<List<Point>> loadPointsFromJsonFile(String name) async {
-    final file = await rootBundle.loadString('assets/golf_courses/$name.json');
-    final jsonData = json.decode(file);
+  Widget _buildGolfCourseInfo() {
+    return FutureBuilder<List<Point>>(
+      future: _loadPointsFromJsonFile(golfCourse),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+        if (snapshot.hasData) {
+          area = snapshot.data!;
+          return _buildPlayerInfo();
+        }
+        return const Text('No area data available.');
+      },
+    );
+  }
 
-    List<Point> points = [];
+  Widget _buildPlayerInfo() {
+    return FutureBuilder<void>(
+      future: _position,
+      builder: (context, positionSnapshot) {
+        if (positionSnapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+        if (positionSnapshot.hasError) {
+          return Text('Error: ${positionSnapshot.error}');
+        }
 
-    for (var point in jsonData['Holes'][0]['area']) {
-      final double latitude = point[0];
-      final double longitude = point[1];
-      points.add(Point(latitude, longitude));
+        final position = positionSnapshot.data as Position;
+        final Point currentPoint = Point(position.latitude, position.longitude);
+
+        if (isPointInArea(currentPoint, area)) {
+          return FutureBuilder<Point>(
+            future: _loadAimPointFromJsonFile(golfCourse),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+              if (snapshot.hasData) {
+                final aimPoint = snapshot.data!;
+                final distance = Geolocator.distanceBetween(
+                  position.latitude,
+                  position.longitude,
+                  aimPoint.latitude,
+                  aimPoint.longitude,
+                );
+                return _buildClubRecommendation(distance);
+              }
+              return const Text('No aim point data available.');
+            },
+          );
+        } else {
+          return const Text('You are outside the golf course area.');
+        }
+      },
+    );
+  }
+
+  Widget _buildClubRecommendation(double distance) {
+    return FutureBuilder<void>(
+      future: _loadPlayerData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator(); // Loading spinner
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          int clubIndex = player.maxDistances.lastIndexWhere((d) => d >= distance);
+          print(clubIndex);
+          if (clubIndex == -1) {
+            clubIndex = 0; // Default to the first club if none match
+          }
+
+          return Text('You should use your ${player.clubs[clubIndex]}');
+        }
+      },
+    );
+  }
+
+  Future<List<Point>> _loadPointsFromJsonFile(String courseName) async {
+    final data =
+        await rootBundle.loadString('assets/golf_courses/$courseName.json');
+    final jsonData = json.decode(data);
+
+    return [
+      for (var point in jsonData['Holes'][0]['area']) Point(point[0], point[1])
+    ];
+  }
+
+  Future<Point> _loadAimPointFromJsonFile(String courseName) async {
+    final data =
+        await rootBundle.loadString('assets/golf_courses/$courseName.json');
+    final jsonData = json.decode(data);
+
+    final latitude = jsonData['Holes'][0]['aim'][0];
+    final longitude = jsonData['Holes'][0]['aim'][1];
+
+    return Point(latitude, longitude);
+  }
+
+  Future<void> _loadPlayerData() async {
+    try {
+      final String data = await rootBundle.loadString('assets/player.json');
+      final Map<String, dynamic> json = jsonDecode(data);
+      // Ensure that player is assigned only once data is fully loaded
+      player = Player.fromJson(json);
+    } catch (e) {
+      throw Exception("Error loading player data: $e");
     }
-
-    return points;
   }
 }
